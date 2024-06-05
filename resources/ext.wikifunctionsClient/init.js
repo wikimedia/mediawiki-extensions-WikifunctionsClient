7ded02da61ec03d39e878e78bca3ce95683931b2
James D. Forrester <jforrester@wikimedia.org>
2024-06-05 10:47:45 -0400

    Initial transfer of code from WikiLambda extension
    Change-Id: I529804ceb167e480c711cc82d896db8a4e6800d6

---
+++ resources/ext.wikifunctionsClient/init.js
@@ -0,0 +1,7 @@
+/**
+ * @class mw.wikifunctionsClient
+ * @singleton
+ */
+mw.wikifunctionsClient = {
+	// Code goes here.
+};
---
+++ tests/phpunit/integration/HooksTest.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * WikifunctionsClient integration test suite for Parser-related hooks
+ *
+ * @copyright 2020– Abstract Wikipedia team; see AUTHORS.txt
+ * @license MIT
+ */
+
+namespace MediaWiki\Extension\WikifunctionsClient\Tests\Integration;
+
+use MediaWiki\Parser\Parser;
+use MediaWikiIntegrationTestCase;
+
+/**
+ * @covers \MediaWiki\Extension\WikifunctionsClient\Hooks
+ * @group Database
+ */
+class HooksTest extends MediaWikiIntegrationTestCase {
+
+	public function testOnParserFirstCallInit() {
+		// Force-enable our code
+		$this->setMwGlobals( [
+			'wgWikifunctionsClientEnableParserFunction' => true,
+		] );
+
+		$parser = $this->createMock( Parser::class );
+		$parser->expects( $this->once() )
+			->method( 'setFunctionHook' )
+			->with( 'function', $this->isType( 'callable' ) );
+
+		$hooks = new \MediaWiki\Extension\WikifunctionsClient\Hooks();
+		$hooks->onParserFirstCallInit( $parser );
+	}
+}
